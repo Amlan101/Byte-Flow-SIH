5ad2ff688b051ff8610d7f0bc8d06af395589e4b
Amlan101 <amlan.sahooricky@gmail.com>
2024-09-03 20:09:56 +0530

    Added the Input Screen

--- app/src/main/java/com/example/byteflowsih/activities/InputActivity.kt
+++ app/src/main/java/com/example/byteflowsih/activities/InputActivity.kt
@@ -1,6 +1,8 @@
 package com.example.byteflowsih.activities
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,9 @@ class InputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            Toast.makeText(this, "Submitted Data", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/byteflowsih/activities/MainActivity.kt
+++ app/src/main/java/com/example/byteflowsih/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.byteflowsih.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.byteflowsih.R
 
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login = findViewById<Button>(R.id.button)
+
+        login.setOnClickListener {
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
