7f57c2500fb067014dc7cd28e624c8aa508b38ea
Amlan101 <amlan.sahooricky@gmail.com>
2024-09-03 19:20:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/byteflowsih/activities/InputActivity.kt
@@ -0,0 +1,15 @@
+package com.example.byteflowsih.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.byteflowsih.R
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
