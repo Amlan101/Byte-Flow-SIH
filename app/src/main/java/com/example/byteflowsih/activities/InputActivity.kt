d5f449cc1b906f9ccaced6d332592e03789b9819
Amlan101 <amlan.sahooricky@gmail.com>
2024-09-14 19:49:00 +0530

    Changed all the input fields according to the ML model's inputs and handled the logic accordingly

--- app/src/main/java/com/example/byteflowsih/activities/InputActivity.kt
+++ app/src/main/java/com/example/byteflowsih/activities/InputActivity.kt
@@ -33,25 +33,20 @@ class InputActivity : AppCompatActivity() {
     private fun submitData() {
 
         // Collecting data from user
-        val excavationInput =
-            findViewById<EditText>(R.id.inputExcavation).text.toString().toDoubleOrNull()
-        val transportationInput =
-            findViewById<EditText>(R.id.inputTransportation).text.toString().toDoubleOrNull()
-        val equipmentInput =
-            findViewById<EditText>(R.id.inputEquipment).text.toString().toDoubleOrNull()
-        val methaneInput =
-            findViewById<EditText>(R.id.inputMethane).text.toString().toDoubleOrNull()
+        val equipmentEmissionsInput = findViewById<EditText>(R.id.inputEquipmentEmissions).text.toString().toDoubleOrNull()
+        val electricalEmissionsInput = findViewById<EditText>(R.id.inputElectricalEmissions).text.toString().toDoubleOrNull()
+        val methaneInput = findViewById<EditText>(R.id.inputMethane).text.toString().toDoubleOrNull()
+        val scope3EmissionsInput = findViewById<EditText>(R.id.inputScope3).text.toString().toDoubleOrNull()
+        val energyConsumptionInput = findViewById<EditText>(R.id.inputEnergyConsumption).text.toString().toDoubleOrNull()
+        val fuelConsumptionInput = findViewById<EditText>(R.id.inputFuelConsumption).text.toString().toDoubleOrNull()
         val selectedState = findViewById<Spinner>(R.id.spinner).selectedItem.toString()
 
-        // Validating inputs
-        if (excavationInput == null || transportationInput == null || equipmentInput == null || methaneInput == null) {
-            Toast.makeText(this, "Please enter valid values for all fields", Toast.LENGTH_SHORT)
-                .show()
+        // Performing null check on the inputs
+        if (equipmentEmissionsInput == null || electricalEmissionsInput == null || methaneInput == null ||
+            scope3EmissionsInput == null || energyConsumptionInput == null || fuelConsumptionInput == null) {
+            Toast.makeText(this, "Please enter valid values for all fields", Toast.LENGTH_SHORT).show()
             return
         }
-
-        // Calculate the new carbon emissions
-        val newCarbonEmissions = excavationInput + transportationInput + equipmentInput
 
         // Fetch the existing data for the selected state
         val stateRef = database.child("states").child(selectedState)
@@ -62,19 +57,36 @@ class InputActivity : AppCompatActivity() {
                 val snapshot = task.result
                 if (snapshot.exists()) {
                     // If the state exists, get the current values
-                    val currentCarbonEmissions = snapshot.child("carbonEmissions").getValue(Double::class.java) ?: 0.0
-                    val currentMethaneEmissions = snapshot.child("methaneEmissions").getValue(Double::class.java) ?: 0.0
-                    Log.d("Firebase", "Current Carbon Emissions: $currentCarbonEmissions")
-                    Log.d("Firebase", "Current Methane Emissions: $currentMethaneEmissions")
+                    val currentEquipmentEmissions = snapshot.child("Equipment Emissions").getValue(Double::class.java) ?: 0.0
+                    val currentElectricalEmissions = snapshot.child("Electrical Emissions").getValue(Double::class.java) ?: 0.0
+                    val currentMethaneEmissions = snapshot.child("Methane Emissions").getValue(Double::class.java) ?: 0.0
+                    val currentScope3Emissions = snapshot.child("Scope 3 Emissions").getValue(Double::class.java) ?: 0.0
+                    val currentEnergyConsumption = snapshot.child("Energy Consumption").getValue(Double::class.java) ?: 0.0
+                    val currentFuelConsumption = snapshot.child("Fuel Consumption").getValue(Double::class.java) ?: 0.0
 
-                    // Add new values to the current values
-                    val totalCarbonEmissions = currentCarbonEmissions + newCarbonEmissions
+                    Log.d("Firebase", "Current Methane Emissions: $currentMethaneEmissions")
+                    Log.d("Firebase", "Current Equipment Emissions: $currentEquipmentEmissions")
+                    Log.d("Firebase", "Current Electrical Emissions: $currentElectricalEmissions")
+                    Log.d("Firebase", "Current Scope 3 Emissions: $currentScope3Emissions")
+                    Log.d("Firebase", "Current Energy Consumption: $currentEnergyConsumption")
+                    Log.d("Firebase", "Current Fuel Consumption: $currentFuelConsumption")
+
+                    // Add the new values to the current values
+                    val totalEquipmentEmissions = currentEquipmentEmissions + equipmentEmissionsInput
+                    val totalElectricalEmissions = currentElectricalEmissions + electricalEmissionsInput
                     val totalMethaneEmissions = currentMethaneEmissions + methaneInput
+                    val totalScope3Emissions = currentScope3Emissions + scope3EmissionsInput
+                    val totalEnergyConsumption = currentEnergyConsumption + energyConsumptionInput
+                    val totalFuelConsumption = currentFuelConsumption + fuelConsumptionInput
 
                     // Update the data in Firebase
                     val data = mapOf(
-                        "carbonEmissions" to totalCarbonEmissions,
-                        "methaneEmissions" to totalMethaneEmissions
+                        "Equipment Emissions" to totalEquipmentEmissions,
+                        "Electrical Emissions" to totalElectricalEmissions,
+                        "Methane Emissions" to totalMethaneEmissions,
+                        "Scope 3 Emissions" to totalScope3Emissions,
+                        "Energy Consumption" to totalEnergyConsumption,
+                        "Fuel Consumption" to totalFuelConsumption
                     )
 
                     stateRef.updateChildren(data).addOnCompleteListener { updateTask ->
@@ -87,8 +99,12 @@ class InputActivity : AppCompatActivity() {
                 } else {
                     // If the state does not exist, create a new entry
                     val data = mapOf(
-                        "carbonEmissions" to newCarbonEmissions,
-                        "methaneEmissions" to methaneInput
+                        "Equipment Emissions" to equipmentEmissionsInput,
+                        "Electrical Emissions" to electricalEmissionsInput,
+                        "Methane Emissions" to methaneInput,
+                        "Scope 3 Emissions" to scope3EmissionsInput,
+                        "Energy Consumption" to energyConsumptionInput,
+                        "Fuel Consumption" to fuelConsumptionInput
                     )
 
                     stateRef.setValue(data).addOnCompleteListener { newTask ->
